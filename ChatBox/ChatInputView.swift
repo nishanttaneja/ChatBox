d7ed2b0c46f1cdc0cc0b0088d094e5d2eeb47171
Nishant Taneja <nishant.taneja2701@gmail.com>
2021-05-12 12:55:46 +0530

    UITextView Dynamic Height

--- ChatBox/ChatInputView.swift
+++ ChatBox/ChatInputView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+//MARK:- ActionType
 @objc enum ChatInputViewActionType: Int {
     case addAttachment, startAudioRecording, sendMessage
     
@@ -21,15 +22,18 @@ import UIKit
     }
 }
 
+//MARK:- Delegate
 @objc protocol ChatInputViewDelegate: AnyObject {
     @objc optional func chatInputView(_ view: ChatInputView, didSelectIconFor actionType: ChatInputViewActionType)
 }
 
+//MARK:- DelegateLayout
 @objc protocol ChatInputViewDelegateLayout: AnyObject {
     @objc optional func spacingForItems(in chatInputView: ChatInputView) -> CGFloat
 }
 
-class ChatInputView: UIView {
+//MARK:- ChatInputView
+class ChatInputView: UIView, UITextViewDelegate {
     // Delegates
     weak var delegate: ChatInputViewDelegate?
     weak var delegateLayout: ChatInputViewDelegateLayout? {
@@ -47,8 +51,8 @@ class ChatInputView: UIView {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.backgroundColor = .white
-        textView.font = .systemFont(ofSize: 16)
-        textView.textContainer.lineFragmentPadding = 10
+        textView.font = .systemFont(ofSize: 14)
+        textView.textContainer.lineFragmentPadding = 8
         textView.textColor = .black
         return textView
     }()
@@ -76,29 +80,43 @@ class ChatInputView: UIView {
         delegate?.chatInputView?(self, didSelectIconFor: actionType)
     }
     
+    // UITextView Delegate
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        microphoneAndMessageButton.setImage(ChatInputViewActionType.sendMessage.image, for: .normal)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        microphoneAndMessageButton.setImage(ChatInputViewActionType.startAudioRecording.image, for: .normal)
+    }
+    func textViewDidChange(_ textView: UITextView) {
+        let oldHeight = frame.size.height
+        if textView.contentSize.height > 48 {
+            let newHeight = textView.contentSize.height + 2*padding
+            frame.size.height = newHeight
+            frame.origin.y -= newHeight - oldHeight
+        } else if frame.height > 48 {
+            frame.size.height = 48
+            frame.origin.y -= 48 - oldHeight
+        }
+    }
+    
     // Constraints
     private var constraintsToActivate: [NSLayoutConstraint] {
         [
-            // Attachment Button
-            attachmentsButton.leftAnchor.constraint(equalTo: leftAnchor, constant: padding),
             // Input Text View
             inputTextView.leftAnchor.constraint(equalTo: attachmentsButton.rightAnchor, constant: spacing),
+            inputTextView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            inputTextView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding),
+            // Attachment Button
+            attachmentsButton.leftAnchor.constraint(equalTo: leftAnchor, constant: padding),
+            attachmentsButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -1.5*padding),
+            attachmentsButton.widthAnchor.constraint(equalToConstant: 24),
             // Microphone Button
             microphoneAndMessageButton.leftAnchor.constraint(equalTo: inputTextView.rightAnchor, constant: spacing),
             microphoneAndMessageButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -padding),
-            microphoneAndMessageButton.widthAnchor.constraint(equalTo: attachmentsButton.widthAnchor)
-        ] + topBottomConstraints(for: attachmentsButton, inputTextView, microphoneAndMessageButton)
-    }
-    
-    private func topBottomConstraints(for views: UIView...) -> [NSLayoutConstraint] {
-        var constraints = [NSLayoutConstraint]()
-        for view in views {
-            constraints.append(contentsOf: [
-                view.topAnchor.constraint(equalTo: topAnchor, constant: padding),
-                view.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding)
-            ])
-        }
-        return constraints
+            microphoneAndMessageButton.widthAnchor.constraint(equalToConstant: 24),
+            microphoneAndMessageButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -1.5*padding)
+        ]
     }
         
     // Constructors
@@ -117,21 +135,11 @@ class ChatInputView: UIView {
     // LifeCycle
     override func layoutSubviews() {
         super.layoutSubviews()
-        inputTextView.layer.cornerRadius = inputTextView.frame.height/2
+        inputTextView.layer.cornerRadius = (attachmentsButton.frame.height + 2*padding)/2
     }
     
     override func updateConstraints() {
         super.updateConstraints()
         NSLayoutConstraint.activate(constraintsToActivate)
-    }
-}
-
-extension ChatInputView: UITextViewDelegate {
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        microphoneAndMessageButton.setImage(ChatInputViewActionType.sendMessage.image, for: .normal)
-    }
-    
-    func textViewDidEndEditing(_ textView: UITextView) {
-        microphoneAndMessageButton.setImage(ChatInputViewActionType.startAudioRecording.image, for: .normal)
     }
 }
